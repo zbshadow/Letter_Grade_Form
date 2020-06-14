72b2f3c097eca8b484745427da69ab72d161eaeb
zbshadow <zbshadowx@gmail.com>
2020-06-14 07:13:14 -0400

    Initial Commit Form Design Complete Properties set

---
+++ CalculateLetterGrade/CalculateLetterGrade/frmCalculateGrade.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculateLetterGrade
+{
+    public partial class frmCalculateGrade : Form
+    {
+        public frmCalculateGrade()
+        {
+            InitializeComponent();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtLetterGrade_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
